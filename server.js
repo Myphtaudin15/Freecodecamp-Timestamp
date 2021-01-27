8230de2a8163eaf381829b2317769f774bd6e76e
Myphtaudin15 <71670731+Myphtaudin15@users.noreply.github.com>
2021-01-27 16:22:43 +0800

    Update server.js

--- server.js
+++ server.js
@@ -17,11 +17,31 @@ app.use(express.static('public'));
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
-
+ 
+app.get("/api/timestamp", (req, res => {
+  const date = new Date();
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  })
+}));
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/timestamp/:date_str", function (req, res) {
+  const { date_str } = req.params; 
+  let date = new Date(date_str);
+
+  if (date === null) {
+    return res.json({
+    unix: null,
+    utc: 'Invalid Date'
+    });
+  } else {
+    return res.json({
+      unix: date.getTime(),
+      utc: date.toUTCString()
+    })
+  }
 });
 
 
